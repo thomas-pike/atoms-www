d32f578a503adb2cbcccce267ddff5d0cca3357a
Thomas Pike <tom.pike@xiven.com>
2023-02-25 23:39:10 +0100

    Add PWA support

--- game.js
+++ game.js
@@ -533,3 +533,26 @@ function restart() {
 	}
 	configureGame();
 }
+
+const registerServiceWorker = async () => {
+	if ('serviceWorker' in navigator) {
+		try {
+			const registration = await navigator.serviceWorker.register(
+				'/serviceworker.js',
+				{
+					scope: './',
+				}
+			);
+			if (registration.installing) {
+				console.log('Service worker installing');
+			} else if (registration.waiting) {
+				console.log('Service worker installed');
+			} else if (registration.active) {
+				console.log('Service worker active');
+			}
+		} catch (error) {
+			console.error(`Registration failed with ${error}`);
+		}
+	}
+};
+registerServiceWorker();
